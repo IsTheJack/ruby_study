7a787495a99f9b64f1d4325172628faeea802431
Roberto Oliveira <robertooliveiraestudo@gmail.com>
2020-04-13 11:34:02 -0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# I'm learning Ruby again
+
+### Some links
+
+#### Quick Reference
+
+https://www.zenspider.com/ruby/quickref.html
+
+#### Ruby Cheat Sheet
+
+http://www.testingeducation.org/conference/wtst3_pettichord9.pdf
+
+#### About Blocks, Procs and Lambdas
+
+https://www.rubyguides.com/2016/02/ruby-procs-and-lambdas/
+
---
+++ src/blocks.rb
@@ -0,0 +1,53 @@
+# Good links
+## https://www.rubyguides.com/2016/02/ruby-procs-and-lambdas/
+
+ages = [21, 26, 29, 60, 62, 31, 28]
+
+# Recommended for one line blocks
+double = ages.map { |n| n * 2 }
+
+# Recommended for multiple line blocks
+
+double_when_even = ages.map do |n|
+  is_even = (n % 2).zero?
+  if is_even
+    n * 2
+  else
+    n
+  end
+end
+
+[double, double_when_even].each do |output|
+  puts output
+  puts '------------------'
+end
+
+# Running block inside methods (functions)
+def will_run_block
+  # when you use yield, you'll call the block
+  yield
+  puts 'End'
+end
+
+will_run_block { puts 'Hello' }
+
+# I can verify if a block exists before use it
+def will_verify_before
+  return 'No block' unless block_given?
+
+  yield
+end
+
+puts will_verify_before
+
+# I can work with explicit block passing
+## I don't know why yet but I'll search about the pros and cons
+
+def receive_block(&block)
+  block.call # same as yield
+end
+
+receive_block { puts 'explicit block is interesting' }
+
+# I can store a proc
+# my_prop = Proc.new { }
